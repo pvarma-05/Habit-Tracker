0c0aefef407f381e5356000d1bb366b86d5d4807
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2024-12-18 14:40:39 +0530

    Completed Habits Page Frontend

--- src/app/(dashboard)/habits/page.jsx
+++ src/app/(dashboard)/habits/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -43,6 +43,48 @@ export default function HabitsPage() {
         <br />
         <div className="bottom w-[549px]">
           <p className="text-[#F0F0F0] font-poppins text-base">Add, View, Edit, Delete, and Categorize habits.</p>
+        </div>
+      </section>
+      <section className="flex ml-[49px] mr-[79px] flex-col my-7 gap-5">
+        <div className="flex flex-col gap-3">
+          <h1 className="font-outfit font-semibold text-[35px] text-[#A0FFBA]">VIEW HABITS</h1>
+          <div className="flex justify-between">
+            <div className="flex w-[340px] h-[470px] bg-[#263238] rounded-[29px] justify-center pt-[50px]">
+              <p className="font-poppins font-semibold text-xl">DAILY</p>
+            </div>
+            <div className="flex w-[340px] h-[470px] bg-[#263238] rounded-[29px] justify-center pt-[50px]">
+              <p className="font-poppins font-semibold text-xl">WEEKLY</p>
+            </div>
+            <div className="flex w-[340px] h-[470px] bg-[#263238] rounded-[29px] justify-center pt-[50px]">
+              <p className="font-poppins font-semibold text-xl">MONTHLY</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <Link
+          href={"/login"}
+          className="flex justify-center items-center bg-[#A0FFBA] w-[226px] h-[58px] rounded-[30px] " 
+          >
+            <p className="font-poppins text-base font-semibold text-black">CREATE HABIT</p>
+          </Link>
+          <Link
+          href={"/login"}
+          className="flex justify-center items-center bg-[#fff] w-[226px] h-[58px] rounded-[30px] " 
+          >
+            <p className="font-poppins text-base font-semibold text-black">UPDATE PROGRESS</p>
+          </Link>
+          <Link
+          href={"/login"}
+          className="flex justify-center items-center bg-[#263238] w-[226px] h-[58px] rounded-[30px] " 
+          >
+            <p className="font-poppins text-base font-semibold">EDIT HABIT</p>
+          </Link>
+          <Link
+          href={"/login"}
+          className="flex justify-center items-center bg-[#FB5456] w-[226px] h-[58px] rounded-[30px] " 
+          >
+            <p className="font-poppins text-base font-semibold text-black">DELETE HABIT</p>
+          </Link>
         </div>
       </section>
     </main>
